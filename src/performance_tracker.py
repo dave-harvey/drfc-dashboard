8dab501e69f0142b76d5ac70e9fbe82c88f201f0
David Harvey <davidharvey188@gmail.com>
2025-12-24 16:37:17 +0000

    Added new menu option of performance tracker

---
+++ src/performance_tracker.py
@@ -0,0 +1,106 @@
+from src.helpers import data_source_caption
+import streamlit as st
+import pandas as pd
+from src.plots import scatter_plot
+
+# --------------------------
+# Load data from JSON file
+# --------------------------
+@st.cache_data
+def load_data():
+    df_raw = pd.read_json("data/efl_1.json")
+    df = df_raw.rename(
+        columns={
+            "name": "Team",
+            "shots": "Shots",
+            "shotsAgainst": "Shots_Against",
+            "xG": "xG",
+            "xGA": "xGA",
+            "goals": "Goals",
+            "goalsAgainst": "Goals_Against",
+        }
+    )
+    return df
+
+
+df = load_data()
+
+def render():
+    team_to_highlight = "Doncaster Rovers"
+
+    firstCols = st.columns(1)
+
+    with firstCols[0]:
+        with st.container(border=True):
+            st.markdown(
+                "<h3 style='margin-bottom: 0; text-transform: uppercase;'>Performance Tracker</h3>",
+                unsafe_allow_html=True
+            )
+
+            st.markdown(
+                """
+                We assess Doncaster Rovers’ performance from two complementary perspectives:
+
+                - <span style="color:#00cc44; font-weight:800; text-transform: uppercase;">Frequency:</span>
+                Reflects how often chances are created and how many shots are conceded, capturing the team’s
+                overall attacking and defensive activity.
+
+                - <span style="color:#00cc44; font-weight:800; text-transform: uppercase;">Quality:</span>
+                Measures the standard of those chances using expected goals (xG) for and expected goals against (xGA),
+                indicating how dangerous Doncaster Rovers are in attack and how effectively they limit opposition opportunities.
+                """,
+                unsafe_allow_html=True
+            )
+            # Add a bit of vertical space
+            st.write("")
+
+            data_source_caption()
+
+            # Add a bit of vertical space
+            st.write("")
+
+
+    secondCols = st.columns(1)
+
+    with secondCols[0]:
+        with st.container(border=True):
+            st.markdown(
+                "<h3 style='margin-bottom: 0; text-transform: uppercase;'>Frequency - Shot Volume</h3>",
+                unsafe_allow_html=True
+            )
+
+            # Add a bit of vertical space
+            st.write("")
+
+            scatter_plot(
+                df=df,
+                x="Shots",
+                y="Shots_Against",
+                title="Shots vs Shots Against",
+                xLabel="Average Shots",
+                yLabel="Average Shots Against",
+                highlight_team=team_to_highlight,
+                interactive=False,
+            )
+
+    thirdCols = st.columns(1)
+    with thirdCols[0]:
+        with st.container(border=True):
+            st.markdown(
+                "<h3 style='margin-bottom: 0; text-transform: uppercase;'>Quality - Expected Goals</h3>",
+                unsafe_allow_html=True
+            )
+
+            # Add a bit of vertical space
+            st.write("")
+
+            scatter_plot(
+                df=df,
+                x="xG",
+                y="xGA",
+                title="xG vs xGA",
+                xLabel="Average xG",
+                yLabel="Average xGA",
+                highlight_team=team_to_highlight,
+                interactive=False,
+            )
