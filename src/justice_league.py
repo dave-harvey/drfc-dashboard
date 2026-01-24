abad327412ecc4d48986a50749a950d4253c6880
David Harvey <davidharvey188@gmail.com>
2026-01-24 12:18:10 +0000

    Added bar_chart.py and included in the justice league page

--- src/justice_league.py
+++ src/justice_league.py
@@ -1,20 +1,18 @@
 from src.helpers import data_source_caption
 import streamlit as st
 import pandas as pd
-from src.line_chart import line_chart
+from src.bar_chart import bar_chart
 
 # --------------------------
 # Load data from JSON file
 # --------------------------
 @st.cache_data
 def load_data():
-    df_raw = pd.read_json("data/matches.json")
+    df_raw = pd.read_json("data/efl_1_xpts.json")
     df = df_raw.rename(
         columns={
-            "match": "Match",
-            "xG": "xG",
-            "xGA": "xGA",
-            "xGD": "xGD"
+            "team": "Team",
+            "xPts": "xPts"
         }
     )
     return df
@@ -48,4 +46,25 @@ def render():
             # Add a bit of vertical space
             st.write("")
 
+    secondCols = st.columns(1)
+
+    with secondCols[0]:
+        with st.container(border=True):
+            st.markdown(
+                "<h3 style='margin-bottom: 0; text-transform: uppercase;'>Expected Points League</h3>",
+                unsafe_allow_html=True
+            )
+
+            # Add a bit of vertical space
+            st.write("")
+
+            #
+            bar_chart(
+                df=df,
+                y="Team",
+                x="xPts",
+                title="League 1 2025/26 - Average xPts per Game",
+                value_format="{:.2f}",
+                highlight_label=team_to_highlight
+            )
 
