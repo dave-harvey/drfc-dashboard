6bfd7828112a04d3a55bd3a76a76b375ecce4978
David Harvey <davidharvey188@gmail.com>
2026-02-23 14:53:43 +0000

    Update data

--- src/helpers.py
+++ src/helpers.py
@@ -3,6 +3,8 @@ from datetime import date
 from typing import Union
 
 
+data_date_to_string = st.secrets["DATA_DATE_TO"]
+
 def data_source_caption(
     source: str = "WyScout",
 ):
@@ -13,6 +15,6 @@ def data_source_caption(
         source: Data source name (default: WyScount)
     """
     st.caption(
-        f"Analysis reflects matches played up to and including 17 Feb 2026. Match statistics used for this analysis were sourced from {source}. " +
+        f"Analysis reflects matches played up to and including " + data_date_to_string + ". Match statistics used for this analysis were sourced from {source}. " +
         f"This website is an independent analytical project and is not affiliated with any clubs, leagues, or data providers."
     )
